bc019e4682f6b551665ab2475d7bab422d42edc8
ntrolls <shin.yoo@kaist.ac.kr>
2026-04-08 13:47:19 +0900

    fix triangle

--- triangle.py
+++ triangle.py
@@ -3,9 +3,9 @@ def triangle(a, b, c):
         return -1 #invalid
     elif a + b < c or b + c < a or c + a < b:
          return -1 #invalid
-    elif a == b or b == c or c == a:
-        return 1 # isoceles
     elif a == b and b == c:
         return 2 # equilateral
+    elif a == b or b == c or c == a:
+        return 1 # isoceles
     else:
         return 4
